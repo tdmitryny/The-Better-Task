a432f434094bdfc72e964679ff877cd7dd5c519d
tdmitryny@gmail.com <tdmitryny@gmail.com>
2022-02-24 13:35:11 -0500

    Change header

--- src/components/FirstBox.js
+++ src/components/FirstBox.js
@@ -13,7 +13,7 @@ export function FirstBox() {
         <div className={btn ? "FirstBox" : "FirstBox-hide"}>
             <button className={btn ? "Btn-first-box" : "Btn-first-box-active"} onClick={clickRight}></button>
             <div className="Title-box">
-                <h1 className="Title-inner">💡The Better Task</h1>
+                <h1 className="Title-inner">🌿MicroGreens Task</h1>
             </div>
             <TaskLists />
         </div >
